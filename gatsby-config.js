4f92559a349db4f68670d0c202dbab0de212bffa
Radu M <radu.maier@reason.co>
2019-09-27 09:57:57 +0300

    inital, copy from forked workspace
    https://github.com/radu-reason/gatsby-theme-document/tree/frontmatter-slug-support/gatsby-theme-document

---
+++ gatsby-config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  siteMetadata: {
+    title: `Document by Code Bushi`,
+    name: `Code Bushi`,
+    siteUrl: `https://codebushi.com`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/codebushi`
+      },
+      {
+        name: `twitter`,
+        url: `https://twitter.com/HuntaroSan`
+      }
+    ],
+    sidebarConfig: {
+      forcedNavOrder: ['/'],
+      ignoreIndex: false
+    }
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `content`,
+        name: `content`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          `gatsby-remark-containers`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 704
+            }
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false
+            }
+          },
+          `gatsby-remark-embed-video`
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Document`,
+        short_name: `Document`,
+        start_url: `/`,
+        background_color: `#182952`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/site-icon.png'
+      }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-react-helmet`
+  ]
+};
